57ebc040e3d16152d2f2f2d1f6d2bcab317c1b4e
japara <72559893+japara@users.noreply.github.com>
2022-08-14 12:07:55 +0400

    functional/links

--- main.js
+++ main.js
@@ -21,4 +21,16 @@
 
 
 // ფუტერი 
-    
+  let send = document.querySelector('#send');
+  let value = document.querySelector('#email');
+
+  send.addEventListener('click', check);
+  
+  function check(){
+    if(value.value.length<6){
+        alert("your email is not long enough")
+    }else if(!value.value.includes('@')){
+        alert('Your email needs @ sign')
+    }
+    console.log(value.value);
+  }
