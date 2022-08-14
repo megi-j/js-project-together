f10512ea1ab5e96fdc23db46af6303be5bfccd39
Parsa-29 <74831323+Parsa-29@users.noreply.github.com>
2022-08-14 14:55:02 +0400

    Header

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 // ჰედერი 
 
+document.querySelector('.mobile-navigation').addEventListener('click', function() {
+    document.querySelector('.mobile-nav-content').classList.toggle('nav-active');
+});
 
-
+document.querySelector('.mobile-nav-close').addEventListener('click', function() {
+    document.querySelector('.mobile-nav-content').classList.remove('nav-active');
+});
 
 
 // პირველი სექცია world news 
